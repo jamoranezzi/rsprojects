b540802942f99c9a1fb06d1544a0117f0fbee5a3
Jefferson Andres Morales <jamora@gmail.com>
2016-09-29 14:31:54 -0500

    Nuevo projecto lb4eurecaServer

---
+++ lab4/lab4eurekaserver/src/main/java/com/lab4/Lab4EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4EurekaServerApplication.class, args);
+	}
+}
