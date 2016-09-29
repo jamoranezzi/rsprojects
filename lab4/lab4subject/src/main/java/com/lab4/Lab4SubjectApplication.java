369df70566e10714d0fc66871cdb7d4e9cda077b
Jefferson Andres Morales <jamora@gmail.com>
2016-09-29 15:04:05 -0500

    Commit incial

---
+++ lab4/lab4subject/src/main/java/com/lab4/Lab4SubjectApplication.java
@@ -0,0 +1,12 @@
+package com.lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4SubjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4SubjectApplication.class, args);
+	}
+}
