20bc929df6a8efb1ac82acb141a51492e242fc90
Jefferson Andres Morales <jamora@gmail.com>
2016-09-30 10:48:48 -0500

    terminada ultima version

--- lab4/lab4eurekaclient/runall.sh
+++ lab4/lab4eurekaclient/runall.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+# Ejecuta con mvn todos los profiles configurados (requerido Config Server y EurekaServer)
 mvn spring-boot:run -Dspring.profiles.active=subject &
 mvn spring-boot:run -Dspring.profiles.active=verb &
 mvn spring-boot:run -Dspring.profiles.active=article &
