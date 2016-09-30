954c844321a38907932d2aa96746aed8ba1350cb
Jefferson Andres Morales <jamora@gmail.com>
2016-09-30 10:24:29 -0500

    insert inicial

---
+++ lab4/lab4eurekaclient/src/main/java/com/lab4/Lab4EurekaClientApplication.java
@@ -0,0 +1,12 @@
+package com.lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4EurekaClientApplication.class, args);
+	}
+}
---
+++ lab4/lab4eurekaserver/runall.sh
@@ -0,0 +1,3 @@
+mvn spring-boot:run -Dspring.profiles.active=primary &
+mvn spring-boot:run -Dspring.profiles.active=secondary &
+mvn spring-boot:run -Dspring.profiles.active=tertiary
